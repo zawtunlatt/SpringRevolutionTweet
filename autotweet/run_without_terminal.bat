f35ecb6d6d9457446110ef4eafc219897a81fa1d
Zaw Tun Latt <zawtunlatt91@gmail.com>
2021-06-06 19:30:36 +0900

    @Add   - Configuration files added

---
+++ autotweet/run_without_terminal.bat
@@ -0,0 +1,2 @@
+:: Start the program
+javaw -jar "target\autotweet-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
---
+++ autotweet/stop_the_program.bat
@@ -0,0 +1,3 @@
+:: Kill Java Process
+taskkill /im java.exe /f
+taskkill /im javaw.exe /f
